527b32aac0347200f740a0e22437485242b6dfca
Andy Pohl <andypohl@gmail.com>
2014-06-06 00:43:50 +0200

    Stuff involving hic.  Using the library.

--- bambed.c
+++ bambed.c
@@ -50,7 +50,9 @@ errAbort(
   "   -rg-blacklist=rg1,rg2,...     exclude specfic read-groups (not compatible with -rg-whitelist)\n"
   "   -flag-counts=file             (for debugging) output counts of flags of reads used to file\n"              
   "   -count                        output a bed4 with counts for each region specified\n"
-  "   -hi-c                         output split read bed for Hi-C\n"
+  "   -cc                         output split read bed for chrom capture bams (3C, Hi-C, etc)\n"
+  "   -cc-inter                   output only inter-chromosomal pairs\n"
+  "   -cc-intra                   output only intra-chromosomal pairs\n"
 /*   "   -pe-unpaired                  unpair the paired-end reads (requires -unextended)\n" */
   "   -stranded                     for paired-end reads that are stranded i.e. the first one in\n"
   "                                 the pair should have the strand.\n"
@@ -72,7 +74,9 @@ static struct optionSpec options[] =
    {"include-B-reads", OPTION_BOOLEAN},
    {"include-bad-regions", OPTION_BOOLEAN},
    {"flag-counts", OPTION_STRING},
-   {"hi-c", OPTION_BOOLEAN},
+   {"cc", OPTION_BOOLEAN},
+   {"cc-inter", OPTION_BOOLEAN},
+   {"cc-intra", OPTION_BOOLEAN},
    {"regions", OPTION_STRING},
    {"regions-every", OPTION_INT},
    {"rg-whitelist", OPTION_STRING},
@@ -82,6 +86,13 @@ static struct optionSpec options[] =
    {"verbose", OPTION_BOOLEAN},
    {"fifty", OPTION_BOOLEAN},
    {NULL, 0},
+};
+
+enum hicout
+{
+    both = 0,
+    inter = 1,
+    intra = 2,
 };
 
 void output_bed(struct metaBig *mb, char *outputfile)
@@ -122,57 +133,33 @@ void output_counts(struct metaBig *mb, char *outputfile)
     carefulClose(&output);
 }
 
-void hiCBam(char *bigfile, char *outputfile)
+void output_hic(struct metaBig *mb, char *outputfile)
+/* updated to use pairbed */
 {
-    FILE *out = mustOpen(outputfile, "w");
-    bam1_t *b[2];
-    bam1_t *cur = NULL;
-    bam1_t *pre = NULL;
-    int curr = 0; 
-    boolean has_prev = FALSE;
-    if (!bigfile)
-	errAbort("no file");
-    bamFile in = (strcmp(bigfile, "-") == 0)? bam_dopen(fileno(stdin), "r") : bam_open(bigfile, "r");
-    bam_header_t *header = bam_header_read(in);
-    b[0] = bam_init1();
-    b[1] = bam_init1();
-    while (bam_read1(in, b[curr]) >= 0)
+    FILE *output = mustOpen(outputfile, "w");
+    enum hicout h = both;
+    struct bed *section;
+    if (optionExists("cc-inter"))
+	h = inter;
+    else if (optionExists("cc-intra"))
+	h = intra;    
+    for (section = mb->sections; section != NULL; section = section->next)
     {
-	cur = b[curr];
-	pre = b[1-curr];
-	if (has_prev)
+	struct lm *lm = lmInit(0);
+	struct pairbed *pbList = metaBigPairbedFetch(mb, section->chrom, section->chromStart, section->chromEnd, lm);
+	struct pairbed *pb;
+	for (pb = pbList; pb != NULL; pb = pb->next)
 	{
-	    char *name1 = bam1_qname(cur);
-	    char *name2 = bam1_qname(pre);
-	    if (name1 && name2 && sameString(name1, name2))
-	    /* we have both in a pair of paired-end reads */
+	    if ((h == both) || ((h == inter) && (!sameString(pb->chrom, pb->mChrom))) || 
+		((h == intra) && (sameString(pb->chrom, pb->mChrom))))
 	    {
-		bam1_core_t *core_f = &pre->core;
-		bam1_core_t *core_s = &cur->core;
-		char *chrom_f = header->target_name[core_f->tid];
-		char *chrom_s = header->target_name[core_s->tid];
-		int start_f = core_f->pos;
-		int start_s = core_s->pos;
-		int end_f = bam_calend(core_f, bam1_cigar(pre));
-		int end_s = bam_calend(core_s, bam1_cigar(cur));
-		char strand_f = '+';
-		char strand_s = '+';
-		if (core_f->flag & BAM_FREVERSE)
-		    strand_f = '-';
-		if (core_s->flag & BAM_FREVERSE)
-		    strand_s = '-';
-		fprintf(out, "%s\t%d\t%d\t%c\t%s\t%d\t%d\t%c\n", chrom_f, start_f, end_f, strand_f, chrom_s, start_s, end_s, strand_s);
-		has_prev = FALSE;
+		fprintf(output, "%s\t%d\t%s\t%s\t%d\t%s\n", pb->chrom, pb->chromStart, pb->strand, 
+			pb->mChrom, pb->mChromStart, pb->mstrand);
 	    }
 	}
-	else 
-	    has_prev = TRUE;
-	curr = 1 - curr;
+	lmCleanup(&lm);
     }
-    bam_destroy1(b[0]);
-    bam_destroy1(b[1]);
-    bam_close(in);
-    carefulClose(&out);
+    carefulClose(&output);
 }
 
 void bambed(char *bigfile, char *outputfile)
@@ -180,71 +167,71 @@ void bambed(char *bigfile, char *outputfile)
 {
     char *regionsBed = optionVal("regions", NULL);
     boolean verbose = optionExists("verbose");
-    if (bigfile == NULL)
-	uglyf("WTF\n");
-    if (optionExists("hi-c"))
-	hiCBam(bigfile, outputfile);
-    else
+    if (verbose)
+	printf("loading %s... \n", bigfile);
+    struct metaBig *mb = metaBigOpen(bigfile, regionsBed);
+    if (verbose)
+	printf("loaded %s ok\n", bigfile);
+    boolean do_cc = FALSE;
+    if (optionExists("cc") || optionExists("cc-inter") || optionExists("cc-intra"))
+	do_cc = TRUE;
+    if (optionExists("cc-inter") && optionExists("cc-intra"))
+	errAbort("-cc-inter and -cc-intra can't both be used");
+    if (optionExists("regions-every"))
     {
-	if (verbose)
-	    printf("loading %s... \n", bigfile);
-	struct metaBig *mb = metaBigOpen(bigfile, regionsBed);
-	if (verbose)
-	    printf("loaded %s ok\n", bigfile);
-	if (optionExists("regions-every"))
-	{
-	    if (regionsBed)
-		errAbort("cannot specify -regions-every with -regions");
-	    else
-		mb->sections = metaBig_chopGenome(mb, optionInt("regions-every", 10000));
-	}	    
-	enum metaBigNameType name_type = justADot;
-	char *blacklist = optionVal("rg-blacklist", NULL);
-	char *whitelist = optionVal("rg-whitelist", NULL);
-	if (whitelist && blacklist)
-	    errAbort("cannot use -rg-whitelist with -rg-blacklist");
-	if (blacklist)
-	    metaBigSetRgList(mb, blacklist, TRUE);
-	else if (whitelist)
-	    metaBigSetRgList(mb, whitelist, FALSE);
-	int shift = optionInt("shift", 0);
-	int length = optionInt("length", 0);
-	char strand = 0;
-	char *name_type_s = optionVal("name-type", NULL);
-	if (name_type_s)
-	{
-	    if (sameWord(name_type_s, "seq"))
-		name_type = sequence;
-	    else if (sameWord(name_type_s, "fqh"))
-		name_type = basicName;
-	    else if (sameWord(name_type_s, "qual"))
-		name_type = quality;
-	    else if (sameWord(name_type_s, "dup"))
-		name_type = duplicates;
-	}
-	if (optionExists("strand"))
-	{
-	    char *s = optionVal("strand", NULL);
-	    if (s && (s[0] == '+'))
-		strand = '+';
-	    else
-		strand = '-';
-	}
-	metaBigSetPositionalOptions(mb, length, shift, strand);
-	metaBigSetNameOption(mb, name_type);
-	mb->includeB = optionExists("include-B-reads");
-	mb->includeBadRegions = optionExists("include-bad-regions");
-	mb->useDupes = optionInt("include-duplicates", 0);
-	mb->mapQ = optionInt("mapq", 20);
-	mb->strandedPe = optionExists("stranded");
-	if (optionExists("mapq"))
-	    mb->useMapQ = TRUE;
-	if (optionExists("count"))
-	    output_counts(mb, outputfile);
+	if (regionsBed)
+	    errAbort("cannot specify -regions-every with -regions");
 	else
-	    output_bed(mb, outputfile);
-	metaBigClose(&mb);
+	    mb->sections = metaBig_chopGenome(mb, optionInt("regions-every", 10000));
+    }	    
+    enum metaBigNameType name_type = justADot;
+    char *blacklist = optionVal("rg-blacklist", NULL);
+    char *whitelist = optionVal("rg-whitelist", NULL);
+    if (whitelist && blacklist)
+	errAbort("cannot use -rg-whitelist with -rg-blacklist");
+    if (blacklist)
+	metaBigSetRgList(mb, blacklist, TRUE);
+    else if (whitelist)
+	metaBigSetRgList(mb, whitelist, FALSE);
+    int shift = optionInt("shift", 0);
+    int length = optionInt("length", 0);
+    char strand = 0;
+    char *name_type_s = optionVal("name-type", NULL);
+    if (name_type_s)
+    {
+	if (sameWord(name_type_s, "seq"))
+	    name_type = sequence;
+	else if (sameWord(name_type_s, "fqh"))
+	    name_type = basicName;
+	else if (sameWord(name_type_s, "qual"))
+	    name_type = quality;
+	else if (sameWord(name_type_s, "dup"))
+	    name_type = duplicates;
     }
+    if (optionExists("strand"))
+    {
+	char *s = optionVal("strand", NULL);
+	if (s && (s[0] == '+'))
+	    strand = '+';
+	else
+	    strand = '-';
+    }
+    metaBigSetPositionalOptions(mb, length, shift, strand);
+    metaBigSetNameOption(mb, name_type);
+    mb->includeB = optionExists("include-B-reads");
+    mb->includeBadRegions = optionExists("include-bad-regions");
+    mb->useDupes = optionInt("include-duplicates", 0);
+    mb->mapQ = optionInt("mapq", 20);
+    mb->strandedPe = optionExists("stranded");
+    if (optionExists("mapq"))
+	mb->useMapQ = TRUE;
+    if (do_cc)
+	output_hic(mb, outputfile);
+    else if (optionExists("count"))
+	output_counts(mb, outputfile);
+    else
+	output_bed(mb, outputfile);
+    metaBigClose(&mb);
 }
 
 int main(int argc, char *argv[])
